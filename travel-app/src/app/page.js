0b4f6e71ebbc8613ba869684574f3649c37de18c
biaryd <bianca.rydenhof@gmail.com>
2025-05-25 13:23:12 +0200

    Empty function for handling logic later on

--- travel-app/src/app/page.js
+++ travel-app/src/app/page.js
@@ -1,7 +1,13 @@
 import NavBar from "@/components/NavBar";
+import Search from "@/components/Search";
 
 
 export default function Home() {
+
+  const handleSearch = (searchTerm) => {
+    /* Implementera logik här sedan för både search och  Random Destination */
+  }
+
   return (
     <div >
       <NavBar></NavBar>
