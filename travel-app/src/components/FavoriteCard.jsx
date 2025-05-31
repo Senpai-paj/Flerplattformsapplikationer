15690ffe1c7385e1b437436312a306eb6a4a6702
Ebbski <ebbakarlberg3@gmail.com>
2025-05-31 23:15:26 +0200

    added slideshow. Still some visual flaws and bugs but the main functionality is there

--- travel-app/src/components/FavoriteCard.jsx
+++ travel-app/src/components/FavoriteCard.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 /* Utseende för favorit-cards plus "baksida" av kortet med info om resan */
 
@@ -11,28 +15,43 @@ export default function FavotiteCard() {
         setTrips(storedTrips);
     }, []);
 
+    const carouselSettings = {
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    };
+
     return (
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
             {trips.map((trip) => (
+                <div
+                    key={trip.id}
+                    className="relative bg-white rounded-lg shadow-md overflow-hidden bg-blue-500">
+                    <figure className="w-full h-64">
+                        <Slider {...carouselSettings}>
+                            {trip.images.map((url, idx) => (
+                            <div key={idx}>
+                                <img
+                                src={url}
+                                alt={`Trip ${trip.title} - ${idx}`}
+                                className="w-full h-64 object-cover"
+                                />
+                            </div>
+                            ))}
+                        </Slider>
+                    </figure>
 
-            <div
-            key={trip.id}
-            className="relative bg-white rounded-lg shadow-md overflow-hidden bg-blue-500">
-                <figure>
-                    <img
-                    src={trip.images[0]}
-                    alt={trip.title}
-                    />
-                </figure>
+                    <div className="card-body h-15 text-center">
+                        <h2 className="card-title font-bold text-xl">
+                            {trip.title}
+                        </h2>
+                    </div>
 
-                <div className="card-body h-15">
-                    <h2 className="card-title font-bold text-center text-xl">
-                        {trip.title}
-                    </h2>
                 </div>
-
-            </div>
             ))}
         </div>
     );
