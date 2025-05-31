46f4159afb57b1d595e99b4b04725dbf909b4f57
Ebbski <ebbakarlberg3@gmail.com>
2025-05-31 12:35:23 +0200

    Styling for Form, where the user creates a trip

--- travel-app/src/components/Form.jsx
+++ travel-app/src/components/Form.jsx
@@ -18,8 +18,8 @@ export default function Form({ handleCreate, handleCancel }) {
 
     return (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
-            <div className="bg-white p-8 w-1/3 h-11/12 rounded-lg overflow-y-auto">
-                <h2 className="text-2xl font-bold mb-4">Create New Trip</h2>
+            <div className="bg-white p-8 w-1/2 h-11/12 rounded-lg overflow-y-auto">
+                <h2 className="text-2xl font-bold mb-4 text-orange-500">Create a New Trip</h2>
                 
                 <div className="mb-6">
                     <div className="flex overflow-x-auto gap-4 pb-4 snap-x snap-mandatory">
@@ -39,14 +39,14 @@ export default function Form({ handleCreate, handleCancel }) {
                     <label htmlFor="title" className="block text-sm font-medium text-gray-700">Title</label>
                     <input type="text" id="title" value={title} 
                         onChange={e => setTitle(e.target.value)} 
-                        className="mt-1 p-2 block w-full rounded-md border border-gray-300" 
+                        className="mt-1 p-2 block w-full rounded-md border border-gray-300 focus:border-orange-500 focus:outline-orange-500" 
                     />
                 </div>
                 <div className="mb-4">
                     <label htmlFor="destination" className="block text-sm font-medium text-gray-700">Destination</label>
                     <input type="text" id="destination" value={destination} 
                         onChange={e => setDestination(e.target.value)} 
-                        className="mt-1 p-2 block w-full rounded-md border border-gray-300" 
+                        className="mt-1 p-2 block w-full rounded-md border border-gray-300 focus:border-orange-500 focus:outline-orange-500" 
                     />
                 </div>
                 <div className="mb-4">
@@ -55,16 +55,16 @@ export default function Form({ handleCreate, handleCancel }) {
                         id="description" 
                         value={description} 
                         onChange={e => setDescription(e.target.value)} 
-                        className="mt-1 p-2 block w-full rounded-md border border-gray-300" 
+                        className="mt-1 p-2 h-48 block w-full rounded-md border border-gray-300 focus:border-orange-500 focus:outline-orange-500" 
                     />
                 </div>
                 <div className="flex justify-end">
                     <button onClick={handleCancel} 
-                        className="mr-2 border border-red-500 text-red-500 hover:bg-red-500 hover:text-white font-bold py-2 px-4 rounded">
+                        className="mr-2 bg-pink-100 hover:bg-pink-200 text-pink-500 font-bold py-2 px-4 rounded-full">
                             Cancel
                         </button>
                     <button onClick={handleCreateClick} 
-                        className=" bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                        className="bg-pink-500 hover:bg-pink-700 text-pink-50 font-bold py-2 px-4 rounded-full">
                         Create
                     </button>
                 </div>
